fdc78b7064965bbb2cac4570f33c861b4ab0c089
Ankit Kumar Verma <kumarvermaankit9555@gmail.com>
2024-02-03 21:18:42 +0530

    change title

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -6,9 +6,9 @@ import FeaturedProducts from "@modules/home/components/featured-products"
 import Hero from "@modules/home/components/hero"
 
 export const metadata: Metadata = {
-  title: "Medusa Next.js Starter Template",
+  title: "bfy",
   description:
-    "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
+    "A store for your tshirt needs",
 }
 
 const getCollectionsWithProducts = async () => {
